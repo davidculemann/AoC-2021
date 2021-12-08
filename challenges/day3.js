edc8148560d9a1975c8297b260baa4ab70170d98
David Culemann <davidculemann@gmail.com>
2021-12-08 17:09:29 +0400

    day 3 part 2

--- challenges/day3.js
+++ challenges/day3.js
@@ -1,93 +1,93 @@
 let lines = require('fs').readFileSync("./data/input3.txt", 'utf-8')
     .split('\n')
 
-// console.log(lines)
+console.log(lines)
 
-// let gammaRate = ""
-// let epsilonRate = ""
+function powerConsumption(lines) {
+    let gammaRate = ""
+    let epsilonRate = ""
 
-// for (let i = 0; i < 12; i++) {
-//     let oneOccurence = 0
-//     let zeroOccurence = 0
+    for (let i = 0; i < 12; i++) {
+        let oneOccurence = 0
+        let zeroOccurence = 0
 
-//     for (const line of lines) {
-//         if (line[i] === '1') {
-//             oneOccurence++
-//         }
-//         else { zeroOccurence++ }
-//     }
-
-//     if (oneOccurence > zeroOccurence) {
-//         gammaRate += '1'
-//         epsilonRate += '0'
-//     }
-//     else {
-//         gammaRate += '0'
-//         epsilonRate += '1'
-//     }
-// }
-// console.log(epsilonRate, gammaRate)
-// console.log(parseInt(gammaRate, 2) * parseInt(epsilonRate, 2))
-
-//=======================================
-
-let CO2rating = ""
-let O2rating = ""
-let O2lines = [...lines]
-let CO2lines = [...lines]
-
-//oxygen rating (most common bit)
-let O2counter = 0
-while (O2lines.length > 1) {
-    let oneOccurence = 0
-    let zeroOccurence = 0
-    let mostCommon = ''
-
-    for (const line of O2lines) {
-        if (line[O2counter] === '1') {
-            oneOccurence++
+        for (const line of lines) {
+            if (line[i] === '1') {
+                oneOccurence++
+            }
+            else { zeroOccurence++ }
         }
-        else { zeroOccurence++ }
-    }
 
-    if (oneOccurence > zeroOccurence) {
-        mostCommon = '1'
+        if (oneOccurence > zeroOccurence) {
+            gammaRate += '1'
+            epsilonRate += '0'
+        }
+        else {
+            gammaRate += '0'
+            epsilonRate += '1'
+        }
     }
-    else if (oneOccurence < zeroOccurence) {
-        mostCommon = '0'
-    }
-    else { mostCommon = '1' }
-    O2lines = O2lines.filter(e => e[O2counter] === mostCommon)
-    O2counter++
-    console.log(O2counter)
+    return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
 }
 
-//CO2 rating (most common bit)
-let CO2counter = 0
-while (CO2lines.length > 1) {
-    let oneOccurence = 0
-    let zeroOccurence = 0
-    let mostCommon = ''
+//===============part 2========================
 
-    for (const line of CO2lines) {
-        if (line[CO2counter] === '1') {
-            oneOccurence++
+function lifeSupportRating(lines) {
+    let O2lines = [...lines]
+    let CO2lines = [...lines]
+
+    //oxygen rating (most common bit)
+    let O2counter = 0
+    while (O2lines.length > 1) {
+        let oneOccurence = 0
+        let zeroOccurence = 0
+        let mostCommon = ''
+
+        for (const line of O2lines) {
+            if (line[O2counter] === '1') {
+                oneOccurence++
+            }
+            else { zeroOccurence++ }
         }
-        else { zeroOccurence++ }
+
+        if (oneOccurence > zeroOccurence) {
+            mostCommon = '1'
+        }
+        else if (oneOccurence < zeroOccurence) {
+            mostCommon = '0'
+        }
+        else { mostCommon = '1' }
+        O2lines = O2lines.filter(e => e[O2counter] === mostCommon)
+        O2counter++
     }
 
-    if (oneOccurence > zeroOccurence) {
-        mostCommon = '1'
+    //CO2 rating (least common bit)
+    let CO2counter = 0
+    while (CO2lines.length > 1) {
+        let oneOccurence = 0
+        let zeroOccurence = 0
+        let leastCommon = ''
+
+        for (const line of CO2lines) {
+            if (line[CO2counter] === '1') {
+                oneOccurence++
+            }
+            else { zeroOccurence++ }
+        }
+
+        if (zeroOccurence < oneOccurence) {
+            leastCommon = '0'
+        }
+        else if (oneOccurence < zeroOccurence) {
+            leastCommon = '1'
+        }
+        else { leastCommon = '0' }
+        if (zeroOccurence !== 0 && oneOccurence !== 0) {
+            CO2lines = CO2lines.filter(e => e[CO2counter] === leastCommon)
+        }
+        CO2counter++
     }
-    else if (oneOccurence < zeroOccurence) {
-        mostCommon = '0'
-    }
-    else { mostCommon = '1' }
-    CO2lines = CO2lines.filter(e => e[CO2counter] !== mostCommon)
-    CO2counter++
-    console.log(CO2lines)
-    console.log(CO2counter)
+    return parseInt(CO2lines[0], 2) * parseInt(O2lines[0], 2)
 }
 
-console.log(O2lines[0])
-console.log(CO2lines)
+console.log(lifeSupportRating(lines))
--- challenges/day5.js
+++ challenges/day5.js
@@ -1,42 +1,6 @@
 let lines = require('fs').readFileSync("./data/input5.txt", 'utf-8')
     .split('\n');
 
-/*
-PSEUDOCODE
-declare an 2d array with 1000 arrays and 1000 0s in each array (GRID)
-take lines and for each element in lines where either x or y is constant, split the element into an array at the arrow
-  within the same array, split the numbers on the comma (which will create a second layer of arrays)
-    for each element of the array, create an object
-      {x1: array[0][0], x2: array[1][0], y1: array[0][1], y2: array[1][1] }
-      if object.x1===objext.x2:
-        for loop from math.min of object.y1, object.y2 to math.max of object.y1, object.y2
-          set grid[i][x1] to be +1
-      else if object.y1===objext.y2:
-        for loop from math.min of object.x1, object.x2 to math.max of object.x1, object.x2
-          set grid[y1][i] to be +1
-
-  INTERSECTION_COUNT=0;
-
-  for every row of GRID:
-    for every element of row:
-      if element > 1:
-        INTERSECTION_COUNT ++;
-
-  [['899', '63'] , ['899', '53']]
-
-  compare the two inside arrays
-    if element[0] of array 0 ===element[0] of array 0, then
-      declare an object which has {y1, y2, x} property
-      y1=> array1[0]
-      y2 => array2[1]
-    else if element[1] of array 1===element[1]of array 1:
-      declare an array xArr made of the 0th array's first element, and the 1st array's first element
-
-
-  ['63', '53', '899']
-  ['899,63' , '899,53']
-*/
-
 function getIntersections(lines) {
     //turn each input coordinate set into an array of two arrays: [[x1, y1], [x2, y2]]
     const linesSplit = lines.map(e => e.split(" -> ").map(el => el.split(",")))
